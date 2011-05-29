ef162b6f9ae3e80852be727864558df7912d7cc2
Nacho Martín <nitram.ohcan@gmail.com>
2011-05-29 05:20:53 +0200

    charts using g.raphael

--- js/le.js
+++ js/le.js
@@ -1,67 +1,12 @@
-Raphael.fn.pieChart = function (cx, cy, r, values, labels, stroke) {
-    var paper = this,
-        rad = Math.PI / 180,
-        chart = this.set();
-    function sector(cx, cy, r, startAngle, endAngle, params) {
-        var x1 = cx + r * Math.cos(-startAngle * rad),
-            x2 = cx + r * Math.cos(-endAngle * rad),
-            y1 = cy + r * Math.sin(-startAngle * rad),
-            y2 = cy + r * Math.sin(-endAngle * rad);
-        return paper.path(["M", cx, cy, "L", x1, y1, "A", r, r, 0, +(endAngle - startAngle > 180), 0, x2, y2, "z"]).attr(params);
-    }
-    var angle = 0,
-        total = 0,
-        start = 0,
-        process = function (j) {
-            var value = values[j],
-                angleplus = 360 * value / total,
-                popangle = angle + (angleplus / 2),
-                color = "hsb(" + start + ", 1, .5)",
-                ms = 500,
-                delta = 30,
-                bcolor = "hsb(" + start + ", 1, 1)",
-                p = sector(cx, cy, r, angle, angle + angleplus, {gradient: "90-" + bcolor + "-" + color, stroke: stroke, "stroke-width": 2}),
-                txt = paper.text(cx + (r + delta + 55) * Math.cos(-popangle * rad), cy + (r + delta + 25) * Math.sin(-popangle * rad), labels[j]).attr({fill: bcolor, stroke: "none", opacity: 0, "font-family": 'Fontin-Sans, Arial', "font-size": "20px"});
-            p.mouseover(function () {
-                p.animate({scale: [1.1, 1.1, cx, cy]}, ms, "elastic");
-                txt.animate({opacity: 1}, ms, "elastic");
-            }).mouseout(function () {
-                p.animate({scale: [1, 1, cx, cy]}, ms, "elastic");
-                txt.animate({opacity: 0}, ms);
-            });
-            angle += angleplus;
-            chart.push(p);
-            chart.push(txt);
-            start += .1;
-        };
-    for (var i = 0, ii = values.length; i < ii; i++) {
-        total += values[i];
-    }
-    for (var i = 0; i < ii; i++) {
-        process(i);
-    }
-    return chart;
-};
-
-
-$(function(){
+$(document).ready(function(){
     window.Data = Backbone.Model.extend({
 
         initialize: function() {
-            if (!this.get("people")) {
-                this.set({"people": 0});
+            if (!this.get("label")) {
+                this.set({title: "undefined"});
             }
-            if (!this.get("votes")) {
-                this.set({"votes": 0});
-            }
-            if (!this.get("blank")) {
-                this.set({"blank": 0});
-            }
-            if (!this.get("invalid")) {
-                this.set({"invalid": 0});
-            }
-            if (!this.get("parties")) {
-                this.set({parties:{}});
+            if (!this.get("amount")) {
+                this.set({amount: 0});
             }
         },
 
@@ -83,17 +28,36 @@ $(function(){
     window.DataList = Backbone.Collection.extend({
         model: Data,
         url: "/data",
+        comparator: function(data) {
+            return data.get("amount");
+        },
     });
+    window.DataStore = new DataList;
 
     window.DataListView = Backbone.View.extend({
         el: $("#holder"),
 
         initialize: function() {
-            _.bindAll(this);
+            _.bindAll(this, "render");
+            DataStore.bind('redraw', this.render);
+            this.paper = Raphael("holder", 800, 800);
+            this.render();
         },
+
+        render: function() {
+            var values = [];
+            var labels = [];
+            DataStore.each(function (data){
+                values.push(data.get('amount'));
+                labels.push(data.get('label'));
+            });
+            this.paper.g.txtattr.font = "12px 'Fontin Sans', Fontin-Sans, sans-serif";
+            this.paper.g.text(320, 100, "Interactive Pie Chart").attr({"font-size": 20});
+            this.paper.g.piechart(350, 350, 200, values, {legend: labels, legendpos: "west", legendothers: "Otros"});
+
+        }
     });
 
-    window.DataStore = new DataList;
     window.App = new DataListView;
 });
 
